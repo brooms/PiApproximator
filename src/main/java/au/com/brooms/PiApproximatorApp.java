86a7a3e40f7b3723ea622e783dbdd781b7368f19
Brooms <broomheada@ONE7-3898SN1.au.fjanz.com>
2015-11-25 16:45:08 +1030

    Updated main method to make the <method> parameter optional.

--- src/main/java/au/com/brooms/PiApproximatorApp.java
+++ src/main/java/au/com/brooms/PiApproximatorApp.java
@@ -38,12 +38,12 @@ public class PiApproximatorApp {
     Approximation approximation = au.com.brooms.DaggerPiApproximatorApp_Approximation.builder().build();
 
     // Check the number of arguments, if the right number of arguments are not present print out usage instructions
-    if (args.length != 3) {
+    if (args.length < 2) {
       System.out.println("--------------------------------------------------------------------------------");
       System.out.println("Incorrect Usage. PiApproximation <numberOfSteps> <numberOfWorkers> <method>");
-      System.out.println("Example: PiApproximation 1000000 10 Madhava-Leibniz");
-      System.out.println("Methods are: Madhava-Leibniz, Gregory-Leibniz, Fabrice-Bellard, Bailey-Borwein-Plouffe");
-      System.out.println("Default approximation methods is Gregory-Leibniz");
+      System.out.println("Example: PiApproximation 1000000 10 Gregory-Leibniz");
+      System.out.println("Methods are: Gregory-Leibniz, Madhava-Leibniz, Fabrice-Bellard, Bailey-Borwein-Plouffe");
+      System.out.println("Default approximation method is Gregory-Leibniz if none is specified.");
       System.out.println("--------------------------------------------------------------------------------");
     } else {
 
@@ -52,8 +52,14 @@ public class PiApproximatorApp {
         long numberOfSteps = Long.parseLong(args[0]);
         int numberOfWorkers = Integer.parseInt(args[1]);
 
+        // Default the estimation method
+        String approximationMethod = "Gregory-Leibniz";
+        if (args.length == 3) {
+          approximationMethod = args[2];
+        }
+
         // Start the approximation calculation
-        approximation.piApproximation().calculate(numberOfSteps, numberOfWorkers, args[2]);
+        approximation.piApproximation().calculate(numberOfSteps, numberOfWorkers, approximationMethod);
 
       } catch (NumberFormatException ex) {
         // Will occur if bad numbers are passed as arguments
